a57c3e643d6960f260ac1868eadcf40ade320a92
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-03 22:36:00 +0300

    Added code for Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+{
+    int M = 1;
+    int N = 15;
+    System.Console.WriteLine("M = " + M + ";N = " + N + "->" + Sum(M, N));
+}
+int Sum(int M, int N)
+{
+    
+    int sum = M + N;
+    if (M == N)
+    {
+        return M;
+    }
+    return sum + Sum(M + 1, N - 1);
+}
+
+
